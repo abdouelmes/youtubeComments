b3e43b639b34646056065647fba99be63219e3ad
abe <abe@totalonion.com>
2022-08-02 21:59:06 +0100

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ function App() {
   // {id: 1, text: 'Doctors app', day: 'Feb 5th at 2:30pm', reminder: true, },
   // {id:2, text:'Dentist app', day:'Feb 6th at 2:30pm',reminder: true,},
   // {id: 3, text: 'school app', day: 'Feb 7th at 2:30pm', reminder: true, }])
-  //delete task
+  //delete task 
+  
  
   return (
     <div className="innerContainer">
