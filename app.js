431a99503087b063becffa4d44b786e0ad40987d
abe <abe@totalonion.com>
2022-08-03 18:38:34 +0100

    db added and working

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const conn = require("./db_connection");
+
 const app = express();
-  
+conn.connectDb();
+
 app.post("/post", (req, res) => {
   console.log("Connected to React");
   res.redirect("/");
 });
-  
+
 const PORT = process.env.PORT || 8080;
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
